3e846f740e6306fb14ad374012d80f2ec271e2e0
LeonardoGermanReyes <2110301@utmatamoros.edu.mx>
2025-01-16 23:28:26 -0600

    Finalizacion P1.py
    Ultima parte del código

--- P1.py
+++ P1.py
@@ -69,3 +69,63 @@ def cuadrado_cubo():
         print(f"El Cubo de {num} es: {num ** 3}")
     except ValueError:
         print("Error Por Favor Introduce un Numero Valido")
+
+def promedio():
+    #Se usara "numeros" para crear una lista de los numeros que se agreguen
+    numeros = []
+    while True:
+        try:
+            num = float(input("Introduce un Numero: "))
+            if num == -1:
+                break
+            #La sentencia de ".append" hara que los numeros mientras no sea -1 los vaya guardando en la lista 
+            numeros.append(num)
+        except ValueError:
+            print("Error Por Favor Introduce un Numero Valido.")
+    if numeros:
+        #Se usara la funcion "len" para saber la cantidad de numeros que se ingresaron ya que el promedio
+        #se hace con la suma de los numeros entre el total de estos
+        print(f"El Promedio es: {sum(numeros) / len(numeros)}")
+    else:
+        print("No se Ingresaron Numeros para Calcular el Promedio")
+
+def maximo_minimo():
+    try:
+        numeros = list(map(int, input("Introduce Numeros Enteros Separados por un Espacio: ").split()))
+        if numeros:
+            print(f"El Valor Maximo es: {max(numeros)}")
+            print(f"El Valor Minimo es: {min(numeros)}")
+            print(f"El Total de Numeros Ingresados es: {len(numeros)}")
+        else:
+            print("No se han Ingresado Numeros")
+    except ValueError:
+        print("Error Por Favor Introduce solo Numeros Enteros Validos")
+
+def main():
+    while True:
+        menu()
+        opcion = input("\nSelecciona una Opcion: ")
+        if opcion == '1':
+            suma_numeros()
+        elif opcion == '2':
+            producto_numeros()
+        elif opcion == '3':
+            division()
+        elif opcion == '4':
+            factorial()
+        elif opcion == '5':
+            tablas_multiplicar()
+        elif opcion == '6':
+            cuadrado_cubo()
+        elif opcion == '7':
+            promedio()
+        elif opcion == '8':
+            maximo_minimo()
+        elif opcion == '9':
+            print("Saliendo del programa")
+            break
+        else:
+            print("Opcion no Valida Por Favor Intentalo de Nuevamente")
+
+if __name__ == "__main__":
+    main()
